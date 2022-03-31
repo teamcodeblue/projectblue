6d3116a37db4db89d2d6d07292592f851d567c59
Alec Chae <alecchae@hotmail.com>
2022-03-31 11:37:26 -0600

    Was able to validate request.data using cerberus from the flask server

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-#FLASK SERVER
+# FLASK SERVER
 from flask import Flask
 from flask_cors import CORS
-
+from flask import request
+from cerberus import Validator
+import json
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/api/extension_post')
+@app.route('/api/extension_post', methods=['GET', 'POST'])
 def hello_world():
-   import pymongo
+    schema = {'name': {'type': 'string'}}
+    v = Validator()
+    example = json.loads(request.data)
+    print(v.validate(example, schema))
+    if v.validate(example, schema):
+        import pymongo
+        client = pymongo.MongoClient(
+            "mongodb+srv://projectcodeblue:1234@pcb-23.spovi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        db = client["test_database"]
+        collection = db["test_collection"]
+        collection.insert_one(request.data)
 
-   client = pymongo.MongoClient(
-      "mongodb+srv://projectcodeblue:1234@pcb-23.spovi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-   db = client["test_database"]
+    return
 
-   collection = db["test_collection"]
-
-   example = {"id": 0, "Body": {"url": "www.nike.com", "content": "Sports Wear"}}
-
-   collection.insert_one(example)
-   return
 
 if __name__ == '__main__':
-   app.run(port=30009)
+    # app.debug = True
+    app.run(port=30009)
