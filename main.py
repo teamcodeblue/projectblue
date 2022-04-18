7da928804c5bf232ffd8ead50db48b4d02404336
scottk <templarsigismund@gmail.com>
2022-04-17 20:49:29 -0600

    Added sendback functionality to query for results on the extension

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask import request
 import json
-from model.ContentBasedReccomendation.contentbasedrecommendation import reccomendations
+#from model.ContentBasedReccomendation.contentbasedrecommendation import reccomendations
 
 app = Flask(__name__)
 CORS(app)
@@ -11,7 +11,6 @@ CORS(app)
 
 @app.route('/api/extension_post',methods = ['GET', 'POST'])
 def hello_world():
-   print(request.data)
    import pymongo
    schema = {'url': {'type': 'string'}}
 
@@ -27,7 +26,13 @@ def hello_world():
 
 @app.route('/api/reccomendations_request',methods = ['GET', 'POST'])
 def post_reccomendations():
-   return reccomendations()
+   stringy = {"text" : """2.2004077 Elon Musk says his offer to buy Twitter is about 'the future of civilization,' not making money
+1.7853994 Apple store workers at Grand Central Terminal start collecting signatures to form a union
+1.7619251 Bioengineering more efficient trees for carbon capture
+1.4806573 GitHub Reportedly Suspends Accounts Related to Sanctioned Russian Orgs
+""", "count": 4}
+
+   return json.dumps(stringy)
 
 
 if __name__ == '__main__':
--- webextension/background.js
+++ webextension/background.js
@@ -55,7 +55,7 @@ async function postData(url = '127.0.0.1:30009/api/extension_post', data = {}) {
     },
     redirect: 'follow', // manual, *follow, error
     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify({'url': (tab.url).toString(),'html': getHTML()}) // body data type must match "Content-Type" header
+    body: JSON.stringify({'url': (tab.url).toString(),'html': ""}) // body data type must match "Content-Type" header
   });
   console.log(tab.url);
   return response.json(); // parses JSON response into native JavaScript objects
