cc1dba954b0a123a2ecdde447e3542ccf275c19b
Alec Chae <59631218+alecchae@users.noreply.github.com>
2022-12-08 17:59:44 -0700

    final commit, show latest working and help & tips working as well

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ from flask_cors import CORS
 from flask import request, Response
 import json
 from model.ContentBasedReccomendation.model_defs import ArticleClassifier
+
 cache_run_time = 30
 from model.ContentBasedReccomendation.contentbasedrecommendation import reccomendations, Cache_result
+
 Last_Ran = None
 app = Flask(__name__)
 CORS(app)
+latestRec = None
 
 
 @app.route('/api/extension_post', methods=['GET', 'POST'])
 def hello_world():
-    #print(request.data)
+    # print(request.data)
     import pymongo
-    schema = {'url': {'type': 'string', 'empty': False}, 'timeSpend': {'type': 'float', 'empty': False}, 'html': {'type': 'string', 'empty': False}}
+    schema = {'url': {'type': 'string', 'empty': False}, 'timeSpend': {'type': 'float', 'empty': False},
+              'html': {'type': 'string', 'empty': False}}
     v = Validator(schema)
 
     example = json.loads(request.data)
@@ -31,23 +35,18 @@ def hello_world():
 
 @app.route('/api/reccomendations_request', methods=['GET', 'POST'])
 def post_reccomendations():
-    import time
-    if Last_Ran and Cache_result and Last_Ran + cache_run_time >= time.time():
-        stringy = {"text": Cache_result, "count": 4}
-        return json.dumps(stringy)
-    else:
-        Last_Ran =  time.time()
-        ret_text = reccomendations(model_link="model/ContentBasedRecommendation/oldmodel.pt")
-        stringy = {"text" :ret_text , "count": 4}
+    stringy = {"text": latestRec, "count": 4}
+    return json.dumps(stringy)
 
 
 @app.route('/api/force_reccomendations_request', methods=['GET', 'POST'])
 def force_reccomendations():
-   ret_text = reccomendations(model_link="model/ContentBasedRecommendation/oldmodel.pt")
-   stringy = {"text" :ret_text , "count": 4}
+    ret_text = reccomendations(model_link="model/ContentBasedRecommendation/oldmodel.pt")
+    stringy = {"text": ret_text, "count": 4}
+    global latestRec
+    latestRec = ret_text
+    return json.dumps(stringy)
 
-   return json.dumps(stringy)
+
 if __name__ == '__main__':
     app.run(port=30009)
-
-
--- webextension/popup.js
+++ webextension/popup.js
@@ -1,5 +1,3 @@
-// When the button is clicked
-var global = "The latest recommendation list is empty :(";
 document.addEventListener('DOMContentLoaded', function () {
   var checkbox = document.querySelector('input[type="checkbox"]');
   chrome.storage.sync.get(['state'], function(result) {
@@ -29,56 +27,14 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
-let rec_button = document.getElementById("btn btn-primary");
-let rec_text = document.getElementById("rec_text");
-let latest_rec_button = document.getElementById("latest_rec_button");
-let latest_rec_text = document.getElementById("latest_rec_text");
+//let rec_button = document.getElementById("btn btn-primary");
+//let rec_text = document.getElementById("rec_text");
+//let latest_rec_button = document.getElementById("latest_rec_button");
+//let latest_rec_text = document.getElementById("latest_rec_text");
 // chrome.storage.sync.get("color", ({ color }) => {
 //   changeColor.style.backgroundColor = color;
 // });
 
-latest_rec_button.addEventListener("click", async () => {
-
-async function postData(url = '127.0.0.1:30009/api/extension_post', data = {}) {
-  // Default options are marked with *
-try{
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept' : 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify({'url': "",'html': ""}) // body data type must match "Content-Type" header
-  });
-  //console.log(response.json())
-  return response.json(); // parses JSON response into native JavaScript objects
-  }catch(error)
-  {
-    return null;
-  }
-}
-  let data = await postData('http://127.0.0.1:30009/api/reccomendations_request', { answer: 42 })
-  if(data != null)
-  {
-
-  latest_rec_text.innerHTML =data.text;
-  }
-  else
-  {
-    latest_rec_text.innerHTML = "The latest recommendation list is empty :(";
-  }
-  var blob = new Blob(["Welcome to Websparrow.org."],
-                { type: "text/plain;charset=utf-8" });
-
-  saveAs(blob, "latestRec.txt"); //FileSaver.js
-
-});
 
 rec_button.addEventListener("click", async () => {
 
@@ -116,9 +72,45 @@ try{
   {
     rec_text.innerHTML = 'error, connection failed!';
   }
-  var blob = new Blob(["Welcome to Websparrow.org."],
-                { type: "text/plain;charset=utf-8" });
 
-  saveAs(blob, "latestRec.txt"); //FileSaver.js
+});
+
+latest_rec_button.addEventListener("click", async () => {
+
+async function postData(url = '127.0.0.1:30009/api/extension_post', data = {}) {
+  // Default options are marked with *
+try{
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept' : 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify({'url': "",'html': ""}) // body data type must match "Content-Type" header
+  });
+  //console.log(response.json())
+  return response.json(); // parses JSON response into native JavaScript objects
+  }catch(error)
+  {
+
+    return null;
+  }
+}
+  let data = await postData('http://127.0.0.1:30009/api/reccomendations_request', { answer: 42 })
+  if(data != null)
+  {
+
+  latest_rec_text.innerHTML =data.text;
+  }
+  else
+  {
+    latest_rec_text.innerHTML = "The latest recommendation list is empty :(";
+  }
 
 });
